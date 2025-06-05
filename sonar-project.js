281016fa9eda8e38b915d09a712895e51d811276
vanessakovalsky <v.david@kovalibre.com>
2025-06-05 14:01:29 +0200

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,22 @@
+// sonar-project.js - Configuration pour Node.js
+module.exports = {
+  serverUrl: 'http://localhost:9000',
+  token: process.env.SONAR_TOKEN,
+  options: {
+    'sonar.projectKey': 'nodejs-app',
+    'sonar.projectName': 'NodeJS Application Demo',
+    'sonar.projectVersion': process.env.BUILD_NUMBER || '1.0.0',
+    'sonar.sources': 'src',
+    'sonar.tests': 'test',
+    'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+    'sonar.testExecutionReportPaths': 'test-results.xml',
+    'sonar.coverage.exclusions': [
+      'test/**/*',
+      'coverage/**/*',
+      'node_modules/**/*'
+    ].join(','),
+    'sonar.cpd.exclusions': [
+      'test/**/*'
+    ].join(',')
+  }
+};
